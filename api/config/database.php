abab7f69d97aff30f794b4464852de9e5d188386
SenghoutPeng <speng@paragoniu.edu.kh>
2026-01-16 00:55:42 +0700

    Adding compatibility for master and slave database setup

--- api/config/database.php
+++ api/config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            # 'host' => env('DB_HOST', '127.0.0.1'),
             'read' => [
             'host' => array_filter([
                 env('DB_HOST_SLAVE_1'),
@@ -78,7 +78,19 @@ return [
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            # 'host' => env('DB_HOST', '127.0.0.1'),
+            'read' => [
+                'host' => array_filter([
+                    env('DB_HOST_SLAVE_1'),
+                    env('DB_HOST_SLAVE_2'),
+                ]) ?: ['127.0.0.1'],
+            ],
+
+            'write' => [
+                'host' => [
+                    env('DB_HOST_MASTER', '127.0.0.1'),
+                ],
+            ],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
@@ -98,7 +110,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            # 'host' => env('DB_HOST', '127.0.0.1'),
             'read' => [
                 'host' => array_filter([
                     env('DB_HOST_SLAVE_1'),
