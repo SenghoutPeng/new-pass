bcddc45aa0c311bbe97523fbf2cb426f081a95fc
SenghoutPeng <speng@paragoniu.edu.kh>
2026-02-16 19:52:25 +0700

    Adding branch

--- backend/pages/index.vue
+++
@@ -1,4 +0,0 @@
-<script setup>
-    navigateTo('/admin')
-</script>
-
--- frontend/middleware/org-guest.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  if (process.client) {
-    const token = localStorage.getItem('org_token')          // ← adjust key if needed
-    if (token) {
-      // User already logged in; keep them out of guest pages
-      return navigateTo('/organization/dashboard')
-    }
-  }
-})
