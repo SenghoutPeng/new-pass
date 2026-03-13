ea830186c08f9079a7bd67725be2a097024c5935
VIS <vistis14@proton.me>
2026-03-13 17:51:24 +0700

    Test static Carbon timezone

--- api/app/Http/Controllers/OrganizationController.php
+++ api/app/Http/Controllers/OrganizationController.php
@@ -214,8 +214,8 @@ class OrganizationController extends Controller
         }
 
         // Combine event date and time
-        $eventDateTime = Carbon::parse($eventDate->event_date . ' ' . $eventDate->event_time)->setTimezone(env('APP_TIMEZONE'));
-        $now = Carbon::now(env('APP_TIMEZONE'));
+        $eventDateTime = Carbon::parse($eventDate->event_date . ' ' . $eventDate->event_time)->setTimezone('Asia/Phnom_Penh');
+        $now = Carbon::now('Asia/Phnom_Penh');
 
         // Allow check-in only if event is today and event time has not passed
         if (!$eventDateTime->isToday() || $eventDateTime->lessThan($now)) {
